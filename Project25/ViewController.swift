d2fa6fa3098ee85e829f558150cdefb465e821e8
Aleksandr Veselov <fcaeoff@gmail.com>
2022-09-13 21:11:55 +0500

    Initial Commit

---
+++ Project25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project25
+//
+//  Created by Александр Веселов on 9/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
